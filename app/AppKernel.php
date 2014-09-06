d73116eccd3c4adaf42a5839d6ef9212a85c3a28
nclsHart <contact@nclshart.net>
2014-09-06 17:59:21 +0200

    generate empty ApiBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new OhMyBank\Bundle\ApiBundle\OhMyBankApiBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
---
+++ src/OhMyBank/Bundle/ApiBundle/OhMyBankApiBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace OhMyBank\Bundle\ApiBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OhMyBankApiBundle extends Bundle
+{
+}
