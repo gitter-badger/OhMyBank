61aa0b441b14e42c1454af605fdff145e0a84124
nclsHart <contact@nclshart.net>
2014-09-15 23:41:39 +0200

    add account facade

---
+++ src/OhMyBank/Bundle/ApiBundle/Facade/Account.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace OhMyBank\Bundle\ApiBundle\Facade;
+
+use OhMyBank\Bundle\ApiBundle\Entity\Account as AccountModel;
+
+class Account
+{
+    /**
+     * @var int
+     */
+    public $id;
+
+    /**
+     * @var string
+     */
+    public $name;
+
+    /**
+     * @var float
+     */
+    public $initialBalance;
+
+    /**
+     * @param AccountModel $account
+     */
+    public function __construct(AccountModel $account)
+    {
+        $this->id             = $account->getId();
+        $this->name           = $account->getName();
+        $this->initialBalance = $account->getInitialBalance();
+    }
+}
