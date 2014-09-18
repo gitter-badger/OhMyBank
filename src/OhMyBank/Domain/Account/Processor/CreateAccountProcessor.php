a210e940993558d2c7ae0e5c1bff509e574fd795
nclsHart <contact@nclshart.net>
2014-09-18 23:57:45 +0200

    add create account processor

---
+++ src/OhMyBank/Domain/Account/Processor/CreateAccountProcessor.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace OhMyBank\Domain\Account\Processor;
+
+use OhMyBank\Domain\Account\Action\CreateAccountAction;
+use OhMyBank\Domain\Account\Repository\AccountRepository;
+
+class CreateAccountProcessor
+{
+    /**
+     * @var AccountRepository
+     */
+    protected $repository;
+
+    /**
+     * @param AccountRepository $repository
+     */
+    public function __construct(AccountRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+
+    /**
+     * @param CreateAccountAction $action
+     */
+    public function execute(CreateAccountAction $action)
+    {
+        $account = $this->repository->createNew();
+        $account->setName($action->getName());
+        $account->setInitialBalance($action->getInitialBalance());
+
+        $this->repository->save($account);
+
+        return $account;
+    }
+}
